2424637b5a81cd061f466f3a383c20de824b50e6
Chaithragna12 <aavetichaithragna@gmail.com>
2024-10-26 12:18:27 +0530

    Webhooks Added

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 //app config
 const PORT =process.env.PORT || 4000
@@ -15,6 +16,8 @@ app.use(cors())
 //api routes
 
 app.get('/',(req,res)=>res.send("api working chaithra"))
+
+app.use('/api/user',userRouter)
 
 app.listen(PORT, ()=> console.log(`Server running on port:${PORT}`));
  
