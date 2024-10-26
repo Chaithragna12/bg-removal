efd50e7676e078e3355462a70cadb394630715df
Chaithragna12 <aavetichaithragna@gmail.com>
2024-10-26 18:11:17 +0530

    changed added

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -1,60 +1,58 @@
-import pkg from 'svix';
-const { webhook } = pkg; // ✅ Destructure webhook from the default export
+import {Webhook} from 'svix'// Use capital "W" as per Svix docs
 
 import userModel from '../models/userModel.js'
 //api controller function to manage clerk user with db
 // http://localhost:4000/api/user/webhooks
 
-const clerkWebhooks=async(req,res)=>{
+const clerkWebhooks = async (req, res) => {
     try {
-        //create a sivx instance with clerk 
-        const whook=new webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
-        })
-        const {data,type}=req.body
+        // Verify the webhook signature
+       await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        });
+
+        const { data, type } = req.body;
+
         switch (type) {
-            case "user.created":{
-                const userData={
-                    clerkId:data.id,
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName:data.last_name,
-                    photo:data.image_url
-                }
-                await userModel.create(userData)
-                res.json({})
+            case "user.created": {
+                const userData ={
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                };
+                await userModel.create(userData);
+                res.json({ });
                 break;
             }
             case "user.updated":{
-                const userData={
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName:data.last_name,
-                    photo:data.image_url
-                }
-                await userModel.findOneAndUpdate({clerkId:data.id},userData)
-                res.json({})
+                const userData = {
+                    email: data.email_addresses[0]?.email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                };
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                res.json({});
                 break;
             }
-            case "user.deleted":{
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.json({})
-
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                res.json({ });
                 break;
             }
             default:
                 break;
         }
     } catch (error) {
-        console.log(error.message);
-        res.json({success:false,message:error.message})
-        
+        console.error( error.message);
+        res.json({ success: false, message: error.message });
     }
+};
 
-}
-
-export {clerkWebhooks}
+export { clerkWebhooks };
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+// import punycode from 'punycode/punycode.js';
 
 //app config
 const PORT =process.env.PORT || 4000
